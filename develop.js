a790d104c11371b43da7407d2426bebdf39ca2fa
yegao <familyink@163.com>
2018-08-17 11:05:23 +0800

    1.0.1

--- develop.js
+++
@@ -1,5 +0,0 @@
-export default function(v){
-    return v.replace(/(^\.|[^(\d|\.)])/g, '').replace(/(\d+\.)(.*)/, function (s, $1, $2) {
-        return $1 + $2.replace('.', '')
-    }).replace(/^0([^\.])/g, '$1');
-}
